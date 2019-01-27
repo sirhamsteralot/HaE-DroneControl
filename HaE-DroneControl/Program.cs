263f9f54aeb8411093ae31c232cb5c350d6729cc
sirhamsteralot <sirhamsteralot@gmail.com>
2019-01-27 03:33:51 +0100

    Autopilot integration

--- HaE-DroneControl/Program.cs
+++ HaE-DroneControl/Program.cs
@@ -19,12 +19,53 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
+        public string controllerName { get { return (string)iniSerializer.GetValue("controllerName"); } }
+
         EntityTracking_Module entityTracking;
-        Autopilot_Module autopilotModule;
+        AutoPilot autoPilot;
+        IngameTime time;
+        Scheduler scheduler;
+
+        INISerializer iniSerializer;
+
+        IMyShipController controller;
+
+
+        GridTerminalSystemUtils GTS;
 
         public Program()
         {
+            scheduler = new Scheduler();
+            scheduler.AddTask(Init());
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
+        }
 
+        public IEnumerator<bool> Init()
+        {
+            iniSerializer = new INISerializer("DroneControl");
+            iniSerializer.AddValue("controllerName", x => x, "Controller");
+
+            if (Me.CustomData == "")
+            {
+                string temp = Me.CustomData;
+                iniSerializer.FirstSerialization(ref temp);
+                Me.CustomData = temp;
+            }
+            else
+            {
+                iniSerializer.DeSerialize(Me.CustomData);
+            }
+            yield return true;
+
+            time = new IngameTime();
+            GTS = new GridTerminalSystemUtils(Me, GridTerminalSystem);
+            yield return true;
+
+            controller = GTS.GetBlockWithNameOnGrid(controllerName) as IMyShipController;
+            yield return true;
+
+            entityTracking = new EntityTracking_Module(GTS, controller, null, EntityTracking_Module.refExpSettings.Turret);
+            autoPilot = new AutoPilot(GTS, controller, time, entityTracking);
         }
 
         public void Save()
@@ -34,7 +75,16 @@ namespace IngameScript
 
         public void Main(string argument, UpdateType updateSource)
         {
+            if ((updateSource & UpdateType.Update1) != 0)
+                Update1();
 
+            autoPilot.Main();
+        }
+
+        public void Update1()
+        {
+            time.Tick();
+            scheduler.Main();
         }
     }
 }
